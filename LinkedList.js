b46bf5b24eda863cf367379bc2c060b534d401b1
Keen Wilson <kwilson@finsignum.com>
2022-01-11 17:32:53 -0800

    Linked Lists: implement size()

--- LinkedList.js
+++ LinkedList.js
@@ -2,146 +2,170 @@
 // and a pointer to the next node in the list.
 class Node {
   constructor(value) {
-    this.value = value;
-    this.next = null;
+    this.value = value
+    this.next = null
   }
 }
 
 class LinkedList {
+  #size
   constructor() {
-    this.first = null;
-    this.last = null;
+    // declare fields
+    this.first = null
+    this.last = null
+    this.#size = 0
   }
 
-  _isEmpty() {
-    return this.first == null;
+  #isEmpty() {
+    return this.first == null
   }
 
   addLast(item) {
     // create a new node
-    const node = new Node(item);
+    const node = new Node(item)
     // Check if the list is empty
-    if (this._isEmpty()) {
+    if (this.#isEmpty()) {
       // simply create a new node and assign it
       // assign both first and last to this new node
-      this.first = this.last = node;
+      this.first = this.last = node
     } else {
       // assign the node into the next pointer
-      this.last.next = node;
+      this.last.next = node
       // update last to point to this new node
-      this.last = node;
+      this.last = node
     }
+    this.#size++
   }
 
   addFirst(item) {
-    const node = new Node(item);
-    if (this._isEmpty()) {
-      this.first = this.last = node;
+    const node = new Node(item)
+    if (this.#isEmpty()) {
+      this.first = this.last = node
     } else {
       // We want a new node to point to our first node
-      node.next = this.first;
+      node.next = this.first
       // Set the first node to this new node
-      this.first = node;
+      this.first = node
     }
+    this.#size++
   }
 
   indexOf(item) {
     // traverse through this list all the way to the end
     // as soon as we find a value, we return the index of the item
-    let index = 0;
-    let current = this.first;
+    let index = 0
+    let current = this.first
     while (current != null) {
-      if (current.value == item) return index;
+      if (current.value == item) return index
       // otherwise, set current to the next node
       // and increment index
-      current = current.next;
-      index++;
+      current = current.next
+      index++
     }
     // if we cannot find node with this value
-    return -1;
+    return -1
   }
 
   contains(item) {
-    return this.indexOf(item) != -1;
+    return this.indexOf(item) != -1
   }
 
   removeFirst() {
-    if (this._isEmpty()) {
-      throw new CustomException("No Such Element");
+    if (this.#isEmpty()) {
+      throw new CustomException('No Such Element')
     }
 
     // If the list has a single item
     if (this.first == this.last) {
-      this.first = this.last = null;
-      return;
+      this.first = this.last = null
+    } else {
+      // [10 --> 20 --> 30]
+      // We need two different references
+      // i.e., second is pointing to 20
+      let second = this.first.next
+      // now we can remove the link without losing track to a second point
+      // because we have a second variable as a backup
+      this.first.next = null
+      this.first = second
     }
-    // [10 --> 20 --> 30]
-    // We need two different references
-    // i.e., second is pointing to 20
-    let second = this.first.next;
-    // now we can remove the link without losing track to a second point
-    // because we have a second variable as a backup
-    this.first.next = null;
-    this.first = second;
+
+    this.#size--
   }
 
-  _getPrevious(node) {
-    var current = this.first;
+  #getPrevious(node) {
+    var current = this.first
     while (current != null) {
-      if (current.next == node) return current;
+      if (current.next == node) return current
       // otherwise, set the current to point to the next node
-      current = current.next;
+      current = current.next
     }
     // if we traverse the list to the end and we couldn't find the node
-    return null;
+    return null
   }
 
   removeLast() {
-    // [10 --> 20 --> 30]
-    // last --> 30
-    // previous --> 20
-    // find a previos node, keep a reference so that we  can update last
-    var previous = this._getPrevious(this.last);
-    this.last = previous;
-    // remove the link so that garbage collector remove the node in memoey
-    this.last.next = null;
+    if (this.#isEmpty()) {
+      throw new CustomException('No Such Element')
+    }
+
+    // If the list has a single node in this list
+    if (this.first == this.last) {
+      this.first = this.last = null
+    } else {
+      // [10 --> 20 --> 30]
+      // last --> 30
+      // previous --> 20
+      // find a previos node, keep a reference so that we  can update last
+      var previous = this.#getPrevious(this.last)
+      this.last = previous
+      // remove the link so that garbage collector remove the node in memoey
+      this.last.next = null
+    }
+
+    this.#size--
+  }
+
+  // O(n)
+  size() {
+    return this.#size
   }
 }
 
 function CustomException(message) {
-  const error = new Error(message);
-  error.code = "THIS_IS_A_CUSTOM_ERROR_CODE";
-  return error;
+  const error = new Error(message)
+  error.code = 'THIS_IS_A_CUSTOM_ERROR_CODE'
+  return error
 }
 
-CustomException.prototype = Object.create(Error.prototype);
+CustomException.prototype = Object.create(Error.prototype)
 
-var list = new LinkedList();
-list.addLast(10);
-list.addLast(20);
-list.addLast(30);
-console.log(list);
-list.addFirst(5);
-console.log(list);
-console.log(list.indexOf(10));
-console.log(list.indexOf(30));
-console.log(list.indexOf(40));
-console.log(list.contains(40));
-console.log(list.contains(10));
-list.removeFirst();
-console.log(list);
-list.removeLast();
-console.log(list);
-var emptylist = new LinkedList();
+var list = new LinkedList()
+list.addLast(10)
+list.addLast(20)
+list.addLast(30)
+console.log(list)
+list.addFirst(5)
+console.log(list)
+console.log(list.indexOf(10))
+console.log(list.indexOf(30))
+console.log(list.indexOf(40))
+console.log(list.contains(40))
+console.log(list.contains(10))
+list.removeFirst()
+console.log(list)
+list.removeLast()
+console.log(list)
+console.log('THE CURRENT SIZE', list.size())
+var emptylist = new LinkedList()
 try {
-  emptylist.removeFirst();
+  emptylist.removeFirst()
 } catch (error) {
-  console.log(error.code, error.message);
+  console.log(error.code, error.message)
 }
 
-var singleItemList = new LinkedList();
-singleItemList.addLast(10);
-singleItemList.removeFirst();
-console.log("singleItemList", singleItemList);
+var singleItemList = new LinkedList()
+singleItemList.addLast(10)
+singleItemList.removeFirst()
+console.log('singleItemList', singleItemList)
 
 // To run: node LinkedList.js
