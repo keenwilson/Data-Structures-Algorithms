06986fff20940b4929b4d1347b92600584d2f620
Keen Wilson <kwilson@finsignum.com>
2022-01-13 12:32:32 -0800

    Stack: balance expressions

--- Stack.js
+++ Stack.js
@@ -38,12 +38,63 @@ class StringReverser {
     return reversedString
   }
 }
-const reversedString = StringReverser.reverse('abcd')
-console.log('reversedString', reversedString)
-try {
-  const noString = StringReverser.reverse('')
-} catch (error) {
-  console.log(error.code, error.message)
-}
+// const reversedString = StringReverser.reverse('abcd')
+// console.log('reversedString', reversedString)
+// try {
+//   const noString = StringReverser.reverse('')
+// } catch (error) {
+//   console.log(error.code, error.message)
+// }
 
+// Balanced Expression
 // Whether the pair and order of bracket are balanced
+function BalandedExpression() {
+  const _leftBrackets = ['(', '<', '{', '[']
+  const _rightBrackets = [')', '>', '}', ']']
+
+  const _isLeftBracket = (char) => {
+    return _leftBrackets.includes(char)
+  }
+  const _isRightBracket = (char) => {
+    return _rightBrackets.includes(char)
+  }
+
+  const _bracketsMatch = (left, right) => {
+    return _leftBrackets.indexOf(left) == _rightBrackets.indexOf(right)
+  }
+
+  const isBalanced = (input) => {
+    const charArray = [...input]
+    let stack = []
+    // Iterate over an input character array string, get each character at a time
+    for (let char of charArray) {
+      if (_isLeftBracket(char)) {
+        // If it is an openning bracket, push it on the top of our stack
+        stack.push(char)
+      }
+      if (_isRightBracket(char)) {
+        // If it is a closing bracket
+        // First, check if the stack is empty
+        if (stack.length === 0) return false
+
+        // Pop an item from the stack
+        // and check if the brackets match
+        let top = stack.pop()
+        if (!_bracketsMatch(top, char)) {
+          return false
+        }
+      }
+    }
+    return stack.length === 0
+  }
+  this.isBalanced = isBalanced
+}
+const myPrototype = new BalandedExpression()
+const firstExample = myPrototype.isBalanced('(([1] + <2>)) [a]')
+console.log('(([1] + <2>)) [a]', firstExample, 'should be true')
+const second = myPrototype.isBalanced('(')
+console.log('(', second, 'should be false')
+const third = myPrototype.isBalanced('[[)]')
+console.log('[[)]', third, 'should be false')
+const forth = myPrototype.isBalanced('{[]}')
+console.log('{[]}', forth, 'should be true')
