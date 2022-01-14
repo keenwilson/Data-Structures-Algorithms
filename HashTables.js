1be1eaa09ef0e78f4bba3ebd09f536103565e4f7
Keen Wilson <kwilson@finsignum.com>
2022-01-14 11:53:58 -0800

    Has Tables: use set to find first repeated character

--- HashTables.js
+++ HashTables.js
@@ -1,5 +1,6 @@
 var Utils = require('./Utils')
 
+// Hash Map
 const employeeMap = {
   1: 'Mosh',
   2: 'Josh',
@@ -25,6 +26,17 @@ const c = containsValue(employeeMap, 'Keen')
 // console.log('Object.keys(obj)', Object.keys(employeeMap))
 // console.log('Object.entries(obj)', Object.entries(employeeMap))
 
+// SET
+// Remove duplicated
+// [1, 2, 3, 3, 2, 1, 4, 5]
+let set = {}
+const numbers = [1, 2, 3, 3, 2, 1, 4, 5]
+for (let number of numbers) {
+  set[number] = true
+}
+console.log('set', set)
+console.log('keys in set', Object.keys(set))
+
 function CharacterFinder() {
   // Find the First Non-repeated Character
   // A Green Apple
@@ -49,8 +61,28 @@ function CharacterFinder() {
     }
     return null
   }
+
+  const findFirstRepeatingChar = (str) => {
+    // Create a set to character and integer
+    let set = {}
+    // Iterate over the sting
+    const chars = str.toLowerCase()
+    for (let char of chars) {
+      set[char] = set.hasOwnProperty(char) ? true : false
+    }
+    for (let ch of chars) {
+      // Look up this character in hash table
+      if (set[ch]) {
+        return ch
+      }
+    }
+    return null
+  }
   this.findFirstNonRepeatingChar = findFirstNonRepeatingChar
+  this.findFirstRepeatingChar = findFirstRepeatingChar
 }
 const charFinder = new CharacterFinder()
-const first = charFinder.findFirstNonRepeatingChar('A Green Apple')
-console.log('first', first)
+const firstNon = charFinder.findFirstNonRepeatingChar('A Green Apple')
+
+const firstRepeating = charFinder.findFirstRepeatingChar('A Green Apple')
+console.log('firstRepeating', firstRepeating)
