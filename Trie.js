038225119398ef7a52de6e0828453096dbab30a7
Keen Wilson <kwilson@finsignum.com>
2022-01-17 17:24:05 -0800

    Trie: implement auto completion

--- Trie.js
+++ Trie.js
@@ -20,7 +20,10 @@ class Node {
   }
 
   getChildren = function () {
-    return Object.keys(this.children)
+    console.log('getChildren', Object.keys(this.children))
+    return Object.keys(this.children).length === 0
+      ? []
+      : Object.keys(this.children)
   }
 
   hasChildren = function () {
@@ -71,6 +74,7 @@ class Trie {
 
       current = current.getChild(ch)
     }
+
     return current.isEndOfWord
   }
 
@@ -80,7 +84,6 @@ class Trie {
 
   _traverse(root) {
     // pre-order: visit the root first
-    console.log(root.value)
     for (let child of root.getChildren()) {
       this._traverse(root.children[child])
     }
@@ -94,12 +97,10 @@ class Trie {
   }
   _remove(root, word, index) {
     if (index === word.length) {
-      console.log('terminate recursion', root.value)
       root.isEndOfWord = false
       return
     }
     const ch = word.charAt(index)
-    console.log('word.charAt(index)', ch)
 
     const child = root.getChild(ch)
     if (!child) {
@@ -112,13 +113,68 @@ class Trie {
     if (!child.hasChildren() && !child.isEndOfWord) {
       root.removeChild(ch)
     }
-    console.log(root.value)
+  }
+
+  findWords(prefix) {
+    let words = []
+    let lastNode = this._findLastNodeOf(prefix)
+    this._findWords(lastNode, prefix, words)
+    return words
+  }
+
+  _findLastNodeOf(prefix) {
+    if (!prefix) {
+      return null
+    }
+    prefix = prefix.toLowerCase()
+    let current = this.root
+    for (let ch of prefix) {
+      let child = current.getChild(ch)
+      if (!child) {
+        return null
+      }
+      current = child
+    }
+    return current
+  }
+
+  _findWords(root, prefix, words) {
+    console.log(
+      '_findWords is called at root of',
+      root.value,
+      'with prefix',
+      prefix,
+      'current words',
+      words,
+    )
+    // pre-order traversal
+    // check if root represents ends of word
+    console.log('checking', root.value, root.isEndOfWord)
+    if (root.isEndOfWord) {
+      words.push(prefix)
+      console.log('end of words --> add to words:', words)
+    }
+
+    for (let child of root.getChildren()) {
+      console.log('root.children[child]', root.children[child])
+      if (root.children[child].value) {
+        // pass the value of the child node as prefix
+        this._findWords(
+          root.children[child],
+          prefix + root.children[child].value,
+          words,
+        )
+      }
+    }
   }
 }
 
 const trie = new Trie()
 trie.insert('car')
+trie.insert('card')
 trie.insert('care')
+trie.insert('careful')
+trie.insert('egg')
 // trie.insert('cat')
 // trie.insert('canada')
 // trie.insert('care')
@@ -128,7 +184,14 @@ trie.insert('care')
 // trie.insert('elephant')
 // trie.insert('fox')
 // trie.insert('fiber')
-console.log('DONE', trie.contains(null))
-trie.remove('car')
-trie.contains('car')
-trie.contains('care')
+// console.log('DONE', trie.contains(null))
+// console.log("trie.contains('car')", trie.contains('car'))
+// console.log("trie.contains('care')", trie.contains('care'))
+let words = trie.findWords('car')
+console.log('car', words)
+
+let words2 = trie.findWords('egg')
+console.log('egg', words2)
+
+let words3 = trie.findWords('')
+console.log('', words3)
