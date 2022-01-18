8c3a574e43859285308a4b9cc5fbc794400ea3c3
Keen Wilson <kwilson@finsignum.com>
2022-01-17 16:30:29 -0800

    Trie: implement a hash table and contains()

--- Trie.js
+++ Trie.js
@@ -1,9 +1,23 @@
-function Node(char) {
-  this.value = char
-  // children are stored as Map, where key is the letter and value is a TrieNode for that letter
-  this.children = Array(26)
-  // false by default, a green node means this flag is true
-  this.isEndOfWord = false
+class Node {
+  constructor(char) {
+    this.value = char
+    // children are stored as Map, where key is the letter and value is a TrieNode for that letter
+    this.children = {}
+    // false by default, a green node means this flag is true
+    this.isEndOfWord = false
+  }
+
+  hasChild = function (ch) {
+    return this.children.hasOwnProperty(ch)
+  }
+
+  addChild = function (ch) {
+    return (this.children[ch] = new Node(ch))
+  }
+
+  getChild = function (ch) {
+    return this.children[ch]
+  }
 }
 
 class Trie {
@@ -16,22 +30,41 @@ class Trie {
     let current = this.root
 
     for (let ch of word) {
-      // calculate an index for each child
-      // subtract the numerical value of character from each other
-      let index = ch.charCodeAt(0) - 'a'.charCodeAt(0)
-
-      if (!current.children[index]) {
+      if (!current.hasChild(ch)) {
         // If we don't have this child,
         // we are going to create it
-        current.children[index] = new Node(ch)
+        current.addChild(ch)
       }
       // Then set current to point to that node
-      current = current.children[index]
+      current = current.getChild(ch)
     }
+    // mark the last inserted character as end of the word
+    current.isEndOfWord = true
+  }
+
+  contains(word) {
+    if (!word) {
+      return false
+    }
+
+    word = word.toLowerCase()
+    
+    let current = this.root
+
+    for (let ch of word) {
+      if (!current.hasChild(ch)) {
+        console.log(`could not find ${ch} in seq uence`)
+        return false
+      }
+
+      current = current.getChild(ch)
+    }
+    return current.isEndOfWord
   }
 }
 
 const trie = new Trie()
 trie.insert('cat')
-trie.insert('can')
-console.log('DONE')
+trie.insert('canada')
+
+console.log('DONE', trie.contains(null))
